8f5cdce12cb2bde3ad549d2d95d4b73ac136f4de
A97Campos <andrewleandro97@gmail.com>
2023-10-16 10:33:16 -0300

    final do módulo gulp

--- Dev Full Stack Python/Módulo 16 - Automação de tarefas com Gulp/gulpfile.js
+++ Dev Full Stack Python/Módulo 16 - Automação de tarefas com Gulp/gulpfile.js
@@ -1,6 +1,40 @@
 const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const obfuscate = require("gulp-obfuscate");
+const imagemin = require("gulp-imagemin");
 
-function teste(callback){
+function comprimeImagens() {
+    return gulp.src("./source/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./build/images"));
+}
+
+function comprimeJavaScript() {
+    return gulp.src("./source/scripts/*.js")
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest("./build/scripts"))
+}
+
+function compilaSass() {
+    return gulp.src("./source/styles/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: "compressed"
+    }))
+    .pipe(sourcemaps.write("./maps"))
+    .pipe(gulp.dest("./build/styles"));
+}
+
+exports.default = function() {
+    gulp.watch("./source/styles/*.scss", {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch("./source/scripts/*.js", {ignoreInitial: false}, gulp.series(comprimeJavaScript));
+    gulp.watch("./source/images/*", {ignoreInitial: false}, gulp.series(comprimeImagens));
+}
+
+/*function teste(callback){
     console.log("Executando o Gulp!");
     callback();
 }
@@ -14,7 +48,13 @@ function dizOi(callback) {
 function tchau() {
     console.log("Tchau Gulp !")
 }
+*/
 
-exports.default = gulp.series(teste, dizOi);
+
+//exports.default = gulp.series(teste, dizOi);
 //exports.default = gulp.parallel(teste, dizOi); faz de forma paralela
-exports.dizOi = dizOi;
+/*exports.dizOi = dizOi;
+exports.sass = compilaSass;*/
+
+/*exports.javascript = comprimeJavaScript;
+exports.images = comprimeImagens;*/
