87d95976754ea51fc4e5ebb7c2f9a29a64e00d34
A97Campos <andrewleandro97@gmail.com>
2023-11-06 13:29:04 -0300

    conclusão da parte hero.

--- Dev Full Stack Python/Módulo 21 - Projeto 4/gulpfile.js
+++ Dev Full Stack Python/Módulo 21 - Projeto 4/gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
 function styles() {
     return gulp.src("src/styles/*.scss").pipe(sass({outputStyle: "compressed"})).pipe(gulp.dest("destino/css"));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src("src/images/**/*").pipe(imagemin()).pipe(gulp.dest("destino/images"));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch("src/styles/*.scss", gulp.parallel(styles))
 };
