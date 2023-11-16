fc3f51356efbf79420207689137997daac1238ab
A97Campos <andrewleandro97@gmail.com>
2023-11-16 18:25:05 -0300

    final do projeto clone Disney+

--- Dev Full Stack Python/Módulo 21 - Projeto 4/gulpfile.js
+++ Dev Full Stack Python/Módulo 21 - Projeto 4/gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+
+function scripts () {
+    return gulp.src("src/script/*.js").pipe(uglify()).pipe(gulp.dest("destino/script"));
+}
 
 function styles() {
     return gulp.src("src/styles/*.scss").pipe(sass({outputStyle: "compressed"})).pipe(gulp.dest("destino/css"));
@@ -10,7 +15,8 @@ function images() {
     return gulp.src("src/images/**/*").pipe(imagemin()).pipe(gulp.dest("destino/images"));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
-    gulp.watch("src/styles/*.scss", gulp.parallel(styles))
+    gulp.watch("src/styles/*.scss", gulp.parallel(styles));
+    gulp.watch("src/script/*.js", gulp.parallel(scripts));
 };
--- Dev Full Stack Python/Módulo 21 - Projeto 4/src/script/faq.js
+++ Dev Full Stack Python/Módulo 21 - Projeto 4/src/script/faq.js
@@ -1,56 +1,52 @@
 function abrirFaq1(){
     const faq1 = document.getElementById("faq1");
-    const img = document.getElementById("myImage")
+    const img = document.getElementById("myImage");
 
     if (faq1.style.display == "block"){
-        faq1.style.display = "none"        
-        img.setAttribute('src', 'src/images/icones/icons8-soma-40.png');
-
+        faq1.style.display = "none";       
+        img.setAttribute('src', 'src/images/icones/icons8-soma-30.png');
     } else {
-        faq1.style.display = "block"
-        img.setAttribute('src', 'src/images/icones/icons8-subtração-40.png');
+        faq1.style.display = "block";
+        img.setAttribute('src', 'src/images/icones/icons8-subtração-30.png');
     }
 }
 
 function abrirFaq2(){
     const faq2 = document.getElementById("faq2");
-    const img = document.getElementById("myImage2")
+    const img = document.getElementById("myImage2");
 
     if (faq2.style.display == "block"){
         faq2.style.display = "none";
-        img.setAttribute('src', 'src/images/icones/icons8-soma-40.png');
-
+        img.setAttribute('src', 'src/images/icones/icons8-soma-30.png');
     } else {
         faq2.style.display = "block";
-        img.setAttribute('src', 'src/images/icones/icons8-subtração-40.png');
+        img.setAttribute('src', 'src/images/icones/icons8-subtração-30.png');
     }
 }
 
 function abrirFaq3(){
     const faq3 = document.getElementById("faq3");
-    const img = document.getElementById("myImage3")
+    const img = document.getElementById("myImage3");
 
     if (faq3.style.display == "block"){
         faq3.style.display = "none";
-        img.setAttribute('src', 'src/images/icones/icons8-soma-40.png');
-
+        img.setAttribute('src', 'src/images/icones/icons8-soma-30.png');
     } else {
         faq3.style.display = "block";
-        img.setAttribute('src', 'src/images/icones/icons8-subtração-40.png');
+        img.setAttribute('src', 'src/images/icones/icons8-subtração-30.png');
     }
 }
 
 function abrirFaq4(){
     const faq4 = document.getElementById("faq4");
-    const img = document.getElementById("myImage4")
+    const img = document.getElementById("myImage4");
 
     if (faq4.style.display == "block"){
         faq4.style.display = "none";
-        img.setAttribute('src', 'src/images/icones/icons8-soma-40.png');
-
+        img.setAttribute('src', 'src/images/icones/icons8-soma-30.png');
     } else {
         faq4.style.display = "block";
-        img.setAttribute('src', 'src/images/icones/icons8-subtração-40.png');
+        img.setAttribute('src', 'src/images/icones/icons8-subtração-30.png');
     }
 }
 
