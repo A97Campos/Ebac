f4ae62377eb63f894d11aa53466836ccc8704618
A97Campos <andrewleandro97@gmail.com>
2023-12-04 13:43:44 -0300

    final do projeto

--- Dev Full Stack Python/Módulo 22 - Landing Page/src/scripts/main.js
+++ Dev Full Stack Python/Módulo 22 - Landing Page/src/scripts/main.js
@@ -1,2 +1,32 @@
 AOS.init();
 
+const dataDoEvento = new Date('Dec 23, 2023 19:00:00');
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const tempoParaEvento = timeStampDoEvento - timeStampAtual;
+
+    const segundos = 1000
+    const minutos = segundos * 60;
+    const horas = minutos * 60;
+    const dias = horas * 24;
+
+    const diasAteOEvento = Math.floor(tempoParaEvento / dias);
+    const horasAteOEvento = Math.floor((tempoParaEvento % dias) / horas);
+    const minutosAteOEvento = Math.floor((tempoParaEvento % horas) / minutos);
+    const segundosAteOEvento = Math.floor((tempoParaEvento % minutos) / segundos);
+
+    const contador = document.getElementById('contador');
+
+    contador.innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (tempoParaEvento < 0){
+        clearInterval(contaHoras);
+        contador.innerHTML = "Evento Expirado.";
+        contador.style.color = "red";
+    };
+
+}, 1000);
