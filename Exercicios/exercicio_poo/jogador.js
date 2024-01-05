f4539e1ce6952c320b97f719e817a3b49627758f
A97Campos <andrewleandro97@gmail.com>
2024-01-05 20:20:27 -0300

    melhora no exercicio

--- Exercicios/exercicio_poo/jogador.js
+++ Exercicios/exercicio_poo/jogador.js
@@ -1,22 +1,33 @@
-function PaisJogador(nacionalidade) {
-    this.nacionalidade = nacionalidade;
-}
-
-function IdadeJogador(idade){
-    this.idade = idade;
-}
-
 function Jogador(nome, time, idade, nacionalidade){
     this.nome = nome;
     this.time = time;
-    PaisJogador.call(this, nacionalidade);
-    IdadeJogador.call(this, idade);
+    this.idade = idade;
+    this.nacionalidade = nacionalidade;
 }
 
-const jogador1 = new Jogador('Cristiano Ronaldo', 'Al-Nassr', 38, 'Portugal');
-const jogador2 = new Jogador('Messi', 'Inter Miami', 36, 'Argentina');
-const jogador3 = new Jogador('Neymar', 'Al-Hilal', 31, 'Brasil');
+class JogadorFutebol extends Jogador{
+    constructor(nome,time,idade,nacionalidade,numeroDeGols, numeroDeAssistencias, cartoesVermelhos, cartoesAmarelos){
+        super(nome,time,idade,nacionalidade);
+        this.numeroDeGols = numeroDeGols;
+        this.numeroDeAssistencias = numeroDeAssistencias;
+        this.cartoesVermelhos = cartoesVermelhos;
+        this.cartoesAmarelos = cartoesAmarelos;
+    }
+}
 
-console.log(jogador1);
-console.log(jogador2);
-console.log(jogador3);
+class JogadorVolei extends Jogador {
+    constructor(nome, time, idade, nacionalidade,numeroDePontos, qauntidadeDeSaques){
+        super(nome,time,idade,nacionalidade);
+        this.numeroDePontos = numeroDePontos;
+        this.qauntidadeDeSaques = qauntidadeDeSaques;
+    }
+}
+
+
+const jogadorFutebol1 = new JogadorFutebol('Cristiano Ronaldo', 'Al-Nassr', 38, 'Português', 53, 20, 1, 0);
+const jogadorFutebol2 = new JogadorFutebol('Messi', 'Inter Miami', 36, 'Argentino', 37, 15, 0, 0);
+const jogadorVolei1 = new JogadorVolei('Lucarelli', ' Gas Sales Bluenergy Piacenza', 31, 'Brasileiro', 20, 7);
+
+console.log(jogadorFutebol1);
+console.log(jogadorFutebol2);
+console.log(jogadorVolei1);
