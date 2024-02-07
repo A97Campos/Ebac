f987e984908fc93e0468e6965de1406c267b8616
A97Campos <andrewleandro97@gmail.com>
2024-02-07 11:15:47 -0300

    conclusão do exercicio_ts

---
+++ Exercicios/exercício módulo 26/exercicio_ts.ts
@@ -0,0 +1,17 @@
+//Tarefa 1
+function multiplicacao(a: number, b: number):number {
+return a*b
+};
+
+const numeros = multiplicacao(5,6);
+
+console.log(`O resultado da multiplicação dos números é: ${numeros}`);
+
+//Tarefa 2
+function dizOla(nome: string): string {
+    return "Olá " + nome;
+};
+
+const saudar = dizOla('Andrew');
+
+console.log(saudar);
