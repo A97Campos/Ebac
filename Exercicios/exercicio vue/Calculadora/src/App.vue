8482957e751a40dfdbe401f06f606b1b3b0573d3
A97Campos <andrewleandro97@gmail.com>
2024-02-23 13:50:41 -0300

    Final do Exercício de Vue proposto

---
+++ Exercicios/exercicio vue/Calculadora/src/App.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { reactive, watch } from 'vue';
+
+const estado = reactive({
+  itens: {
+    num1: '',
+    num2: '',
+    operador: '+',
+    resultado: '',
+  }
+  });
+
+  const calcularResultado = () => {
+    switch (estado.itens.operador) {
+        case '+':
+          estado.itens.resultado = estado.itens.num1 + estado.itens.num2;
+          break;
+        case '-':
+          estado.itens.resultado = estado.itens.num1 - estado.itens.num2;
+          break;
+        case '*':
+          estado.itens.resultado = estado.itens.num1 * estado.itens.num2;
+          break;
+        case '/':
+          estado.itens.resultado = estado.itens.num1 / estado.itens.num2;
+          break;
+        default:
+          break;
+      }
+  };
+
+  watch(()=> estado.itens.num1, calcularResultado);
+  watch(()=> estado.itens.num2, calcularResultado);
+  watch(()=> estado.itens.operador, calcularResultado);
+
+</script>
+
+<template>
+  <form>
+    <section>
+      <input type="number" placeholder="Digite um numero" v-model="estado.itens.num1">
+      <select v-model="estado.itens.operador">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input type="number" placeholder="Digite um numero" v-model="estado.itens.num2">
+      <input type="text" placeholder="Resultado" v-model="estado.itens.resultado" id="resultado">
+    </section>
+  </form>
+</template>
+
+<style scoped>
+form {
+  background: linear-gradient(#3E7FF0, #3E47F0);
+  width: 100vw;
+  height: 100vh;
+}
+
+form section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+form input,
+form select {
+  padding: 16px;
+  border-radius: 5px;
+  border: none;
+  outline: none;
+}
+
+form select {
+  margin: 0 8px;
+}
+
+#resultado{
+  margin-left: 8px;
+  width: 80px;
+}
+</style>
