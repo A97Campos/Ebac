6d0d89104bdc242e9734f95981b1bf36bdacd06a
A97Campos <andrewleandro97@gmail.com>
2024-04-03 10:15:47 -0300

    configurando o ambiente de desenvolvimento

--- Dev Full Stack Python/Módulo 29 - Introdução ao CSS in JS com React/projet-react/src/Teste.tsx
+++ Dev Full Stack Python/Módulo 29 - Introdução ao CSS in JS com React/projet-react/src/Teste.tsx
@@ -1,11 +1,26 @@
 import styled from 'styled-components'
 
-const Botao = styled.button`
-background-color: red;
-`
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+  const Botao = styled.button`
+background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+font-size: ${(props) => props.fontsize || '16px'};
+  `
+
+  const BotaoPerigo = styled(Botao)`
+  background: red;
+  color: #fff;
+  `
 
 function Teste() {
-  return <Botao>Clique aqui</Botao>
+  return <>
+  <Botao principal>Enviar</Botao>
+  <Botao principal={false}>Cancelar</Botao>
+  <BotaoPerigo>Não clique</BotaoPerigo>
+  </>
 }
 
 export default Teste
